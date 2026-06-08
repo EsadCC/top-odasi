f09c5f60e05fdf56aaec305008ed47cbed97c184
EsadCC <9028564@student.zadkine.nl>
2026-06-08 13:02:02 +0200

    sql

--- dbcon.php
+++ dbcon.php
@@ -1,8 +1,8 @@
 <?php
 $server   = "localhost";
 $username = "root";
-$password = "";        // macbook gebruikers vullen hier "root" in
-$db       = "escape-room"; // pas aan als jouw database anders heet
+$password = "";        
+$db       = "escape-room";
 
 try {
     $db_connection = new PDO("mysql:host=$server;dbname=$db;charset=utf8mb4", $username, $password);
