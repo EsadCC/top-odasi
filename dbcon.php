9340759200ee9c73c0543653a2585f5452a0aa6f
EsadCC <9028564@student.zadkine.nl>
2026-06-08 13:14:49 +0200

    perongelijk verwijderd en weer toegevoegd

---
+++ dbcon.php
@@ -0,0 +1,15 @@
+<?php
+function getDB() {
+    $server   = "localhost";
+    $username = "root";
+    $password = "";        
+    $db       = "escape-room"; 
+    try {
+        $db_connection = new PDO("mysql:host=$server;dbname=$db;charset=utf8mb4", $username, $password);
+        $db_connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $db_connection->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+        return $db_connection;
+    } catch (PDOException $e) {
+        die("Verbinding mislukt: " . $e->getMessage());
+    }
+}
